fc389d8ba2042ff39dd9bbedc30b318b3f5f9f08
Dominic Gomez <dominicgomez@users.noreply.github.com>
2019-01-12 05:50:04 -0800

    Counts live neighbors correctly

--- simlife/simlife.py
+++ simlife/simlife.py
@@ -15,7 +15,7 @@ def neighbors(cell):
 
     Returns
     -------
-    list of tuple of int
+    iterable of tuple of int
         The row and column coordinates of `cell`'s eight neighbors.
 
     Examples
@@ -33,6 +33,40 @@ def neighbors(cell):
         for row_offset in range(-1, 2) for col_offset in range(-1, 2)
         if (row_offset, col_offset) != (0, 0)
     ]
+
+
+def population(grid, cells):
+    """Count the live cells.
+
+    Parameters
+    ----------
+    grid : set of tuple of int
+        A unique, unordered collection of the row and column coordinates of the
+        live cells in the universe.
+    cells : iterable of tuple of int
+        The row and column coordinates of the cells to test.
+
+    Returns
+    -------
+    int
+        The number of live cells in `cells`.
+
+    Examples
+    --------
+    >>> g = {(1, 3), (2, 4), (3, 2), (3, 3), (3, 4)}
+
+    >>> cs1 = [(1, 2), (1, 3), (1, 4), (2, 2), (2, 4), (3, 2), (3, 3), (3, 4)]
+
+    >>> cs2 = [(-1, 0), (0, 0), (1, 0)]
+
+    >>> population(g, cs1)
+    5
+
+    >>> population(g, cs2)
+    0
+
+    """
+    return sum([1 for c in cells if c in grid])
 
 
 if __name__ == '__main__':
